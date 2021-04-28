e4cd3ceef0a2c2a92757d92ad7517455056b1497
Alex Adams <alex.adams@hudsonmx.com>
2021-04-27 22:49:47 -0400

    Add Spring Boot Starter

---
+++ src/main/java/com/deadbeef/discordsportsbot/DiscordSportsBotApplication.java
@@ -0,0 +1,13 @@
+package com.deadbeef.discordsportsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordSportsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordSportsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deadbeef/discordsportsbot/DiscordSportsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deadbeef.discordsportsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordSportsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
